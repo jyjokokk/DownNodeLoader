94e1f740fffef30eb0e9be7ca44506e8c46b9398
Jyrki Kokkola <j.j.kokkola@gmail.com>
2025-01-17 00:35:24 +0200

    +eslintrules +prettier +project structure

---
+++ src/cli/cli.ts
@@ -0,0 +1,22 @@
+import { Command } from 'commander'
+import packageJson from '../../package.json'
+
+const version = packageJson.version
+
+const program = new Command()
+
+export function hauskaa(arg = 'world'): string {
+  return arg.toUpperCase()
+}
+
+program
+  .version(version)
+  .name('dnl')
+  .option('-d, --debug', 'enables verbose logging', false)
+  .option('-w, --watch', 'watch for file changes', true)
+  .option('-C, --coverage', 'get test coverage', true)
+  .parse(process.argv)
+
+// Function code for CLI goes here
+
+export default program
