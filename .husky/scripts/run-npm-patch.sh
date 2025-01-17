f47a7506f48b1612c02a3c934d123c727f8bd001
Jyrki Kokkola <j.j.kokkola@gmail.com>
2025-01-17 02:18:06 +0200

    vers

--- .husky/scripts/run-npm-patch.sh
+++ .husky/scripts/run-npm-patch.sh
@@ -3,5 +3,5 @@
 branch=$(git rev-parse --abbrev-ref HEAD)
 if [ "$branch" = "prod" ]; then
   echo "Branch is 'production', updating version in package.json..."
-  npm version patch
+  # npm version patch
 fi
